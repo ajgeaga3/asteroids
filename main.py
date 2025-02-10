8dd997b184c84f1e2ddc6a898388b913c9e9ae2a
aj geaga <ajgeaga1@gmail.com>
2025-02-10 15:17:20 -0800

    feat: Add base game object and player mechanics
    - Create  with the  base class for game objects.
    - Implement  in  inheriting from , with:
      - Rotation and movement logic (using WASD keys)
      - A triangle-based representation for the player ship
    - Update  with player-specific settings (PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED) and screen dimensions (SCREEN_WIDTH, SCREEN_HEIGHT)
    - Update  to initialize pygame, create a Player instance, and integrate it into the main game loop

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 
-import constants
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 import pygame
+from player import Player
 
 def main():
     pygame.init()
     print("Starting asteroids!")
-    print("Screen width:", constants.SCREEN_WIDTH)
-    print("Screen height:",  constants.SCREEN_HEIGHT)
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:",  SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 
+        player.update(dt)
     
     
 if __name__ == "__main__":
