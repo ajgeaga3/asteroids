f3ece0666839a4919f0d95fb4d4554611ec451c6
aj geaga <ajgeaga1@gmail.com>
2025-02-10 16:03:18 -0800

    feat: Add asteroid field with collision detection and game over logic
    - Introduce  class (extending ) to represent individual asteroids.
    - Implement  class to spawn asteroids from random screen edges.
    - Update  with  method for collision detection.
    - Enhance  to:
      - Integrate asteroid spawning with dedicated sprite groups.
      - Check collisions between the player and asteroids, exiting the game upon collision.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 
+
+import sys
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 import pygame
 from player import Player
@@ -15,15 +19,22 @@ def main():
     y = SCREEN_HEIGHT / 2
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables,)
     player = Player(x, y)
-
+    asteroidfield = AsteroidField()  
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         updatables.update(dt)
         screen.fill("black")
+        for asteroid in asteroids:
+            if asteroid.does_collide(player):
+                print("Game over!")
+                sys.exit()
         for obj in drawables:
             obj.draw(screen)
         pygame.display.flip()
